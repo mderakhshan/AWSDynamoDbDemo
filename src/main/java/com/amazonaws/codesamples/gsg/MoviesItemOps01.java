f9cd352056323284883ce4dafce744696bb29e93
Mir Derakhshan <Mir.Derakhshan@bjss.com>
2016-12-01 15:07:54 +0000

    Initial commit

---
+++ src/main/java/com/amazonaws/codesamples/gsg/MoviesItemOps01.java
@@ -0,0 +1,48 @@
+// Copyright 2012-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
+
+package com.amazonaws.codesamples.gsg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class MoviesItemOps01 {
+
+    public static void main(String[] args) throws Exception {
+
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient()
+                .withEndpoint("http://localhost:8000");
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Movies");
+
+        int year = 2015;
+        String title = "The Big New Movie";
+
+        final Map<String, Object> infoMap = new HashMap<String, Object>();
+        infoMap.put("plot",  "Nothing happens at all.");
+        infoMap.put("rating",  0);
+
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table.putItem(new Item()
+                    .withPrimaryKey("year", year, "title", title)
+                    .withMap("info", infoMap));
+
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        } catch (Exception e) {
+            System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
